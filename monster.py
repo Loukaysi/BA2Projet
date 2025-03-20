a9aafee9aa03f55978d3aeda18b57835b38b7660
BenoitDeCajou <153362519+BenoitDeCajou@users.noreply.github.com>
2025-03-20 11:37:28 +0100

    Mouvement des chauves-souris

--- monster.py
+++ monster.py
@@ -39,3 +39,31 @@ class Slime(Monster):
                 self.monster_sprite.change_x = -self.monster_sprite.change_x
                 self.monster_sprite.texture = self.monster_sprite.texture.flip_horizontally()
         self.monster_sprite.strafe(self.monster_sprite.change_x)
+
+
+class Bat(Monster):
+    """Deal with the bat movements"""
+
+    def __init__(self, sprite: arcade.Sprite)-> None:
+        self.monster_sprite = sprite
+    
+    def move(self) -> None:
+        # define the relative position of bat in function of the initial position
+        relative_bat_position = (self.monster_sprite.right,self.monster_sprite.bottom) - self.initial_position
+        # define the relative angle of bat in function of the orientation of sprite
+        relative_angle = math.atan2(relative_bat_position[0], relative_bat_position[1]) - self.monster_sprite.angle
+        # test if the vector size is bigger than the scope of action
+        if math.sqrt(relative_bat_position[0]**2 + relative_bat_position[1]**2) >= 50 :
+            # test if the orientation of the bat is aligned with the angle of relative vector
+            # turn the direction of movement if this is right
+            if (relative_angle) <= 90 and (relative_angle) >= 0 :
+                self.monster_sprite.angle -= 180
+            elif (relative_angle) <= 0 and (relative_angle) >= -90 :
+                self.monster_sprite.angle += 180
+        
+        # Change the orientation of bat randomly with an angle between -2 and 2
+        self.monster_sprite.turn_right(self, random.uniform(-2, 2))
+        # Move the bat with a speed constant (=1) in its orientation 
+        self.monster_sprite.strafe(1)
+
+
